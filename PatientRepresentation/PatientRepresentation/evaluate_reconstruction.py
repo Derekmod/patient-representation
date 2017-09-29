74d21a4521ff6f85b355f87def7d6cbf23497d0e
Derekmod <derekmod@gmail.com>
2017-09-29 16:38:52 -0400

    made inputting compatible with Windows

--- PatientRepresentation/PatientRepresentation/evaluate_reconstruction.py
+++ PatientRepresentation/PatientRepresentation/evaluate_reconstruction.py
@@ -1,9 +1,14 @@
+import os
+
 import dataset as dataset_m
 
 
-
-
-
 if __name__ == '__main__':
-    dataset = dataset_m.loadFromDir('../../data/V7 Data', verbose=True)
+    basedir = os.path.dirname(os.getcwd())
+    basedir = os.path.dirname(basedir)
+
+    data_dir = os.path.join(base_dir, 'data')
+    data_dir = os.path.join(base_dir, 'V7 Data')
+
+    dataset = dataset_m.loadFromDir(data_dir, verbose=True)
     print dataset
