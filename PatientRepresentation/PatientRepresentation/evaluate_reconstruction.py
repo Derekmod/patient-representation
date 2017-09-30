30a0a43a155b710b9a7cc3e0c5522ce9d0bca5d4
Derekmod <derekmod@gmail.com>
2017-09-30 13:46:38 -0400

    fix typo

--- PatientRepresentation/PatientRepresentation/evaluate_reconstruction.py
+++ PatientRepresentation/PatientRepresentation/evaluate_reconstruction.py
@@ -5,8 +5,8 @@ from model import PatientModel
 
 
 if __name__ == '__main__':
-    basedir = os.path.dirname(os.getcwd())
-    basedir = os.path.dirname(basedir)
+    base_dir = os.path.dirname(os.getcwd())
+    base_dir = os.path.dirname(base_dir)
 
     data_dir = os.path.join(base_dir, 'data')
     data_dir = os.path.join(base_dir, 'V7 Data')
