error fix with normalizing

PatientRepresentation/PatientRepresentation/model.py
@@ -43,13 +43,14 @@ class PatientModel(object):
 
         pat_mat = self.patient_mat
         print 'mean={}'.format(np.mean(pat_mat, axis=0))
-        print 'moment2={}'.format(pat_mat.T.dot(pat_mat))
+        print 'moment2={}'.format(pat_mat.T.dot(pat_mat)/self.num_patients)
         print 'normalizing'
+        print self.errorFrac(dataset)
         self.normalize()
         print self.errorFrac(dataset)
         pat_mat = self.patient_mat
         print 'mean={}'.format(np.mean(pat_mat, axis=0))
-        print 'moment2={}'.format(pat_mat.T.dot(pat_mat))
+        print 'moment2={}'.format(pat_mat.T.dot(pat_mat)/self.num_patients)
 
 
     def train_transforms(self, dataset):
@@ -102,17 +103,16 @@ class PatientModel(object):
             self.tissue_centers[tissue_name] = -sum_residual/tissue.numPatients
 
     def normalize(self):
-        reps = np.concatenate([self._patient_reps[id] for id in self._patient_reps], axis=0)
-        patient_mean = np.mean(reps, axis=0)
+        patient_mean = np.mean(self.patient_mat, axis=0)
 
         for tissue_name in self.tissues:
-            self._tissue_centers[tissue_name] -= patient_mean.dot(self._tissue_transforms[tissue_name])
+            self._tissue_centers[tissue_name] += patient_mean.dot(self._tissue_transforms[tissue_name])
 
         for patient_id in self.patients:
             self.patient_reps[patient_id] -= patient_mean
 
         # normalize variance
-        reps -= patient_mean
+        reps = self.patient_mat
         patient_cov = reps.T.dot(reps)/self.num_patients
         U, W, V = np.linalg.svd(patient_cov)
         Einv = U.dot(np.diag(W**-.5))